6a437efc1b17b92071f4026ee71aa2993f59710d
RDeBont <rowandebont@outlook.com>
2025-09-10 14:09:10 +0200

    Ticket 7 link naar handleiding

--- resources/views/pages/manual_list.blade.php
+++ resources/views/pages/manual_list.blade.php
@@ -5,25 +5,32 @@
     </x-slot:head>
 
     <x-slot:breadcrumb>
-        <li><a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/" alt="Manuals for '{{$brand->name}}'" title="Manuals for '{{$brand->name}}'">{{ $brand->name }}</a></li>
+        <li><a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/" alt="Manuals for '{{$brand->name}}'"
+                title="Manuals for '{{$brand->name}}'">{{ $brand->name }}</a></li>
     </x-slot:breadcrumb>
 
 
     <h1>{{ $brand->name }}</h1>
 
-    <p>{{ __('introduction_texts.type_list', ['brand'=>$brand->name]) }}</p>
+    <p>{{ __('introduction_texts.type_list', ['brand' => $brand->name]) }}</p>
 
 
-        @foreach ($manuals as $manual)
 
+    @foreach ($manuals as $manual)
+        <div class="manualButten">
             @if ($manual->locally_available)
-                <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/{{ $manual->id }}/" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
+                <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/{{ $manual->id }}/"
+                    alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
                 ({{$manual->filesize_human_readable}})
             @else
-                <a href="{{ $manual->url }}" target="new" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
+                <a href="{{ $manual->url }}" target="new" alt="{{ $manual->name }}"
+                    title="{{ $manual->name }}">{{ $manual->name }}</a>
             @endif
 
             <br />
-        @endforeach
+        </div>
+    @endforeach
+    <div class="clear"></div>
+
 
 </x-layouts.app>
