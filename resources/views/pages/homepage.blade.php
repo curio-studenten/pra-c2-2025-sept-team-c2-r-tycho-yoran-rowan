133896b47bb2a6f5135fe577e9e26a566bf1160c
Yoran <49372904+Y0RAN@users.noreply.github.com>
2025-09-11 12:45:33 +0200

    tijdelijke grid

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -49,37 +49,52 @@ $topSplit = 1;
         <!-- Example row of columns -->
         <p>{{ $description }}</p>
         <div class="row">
+            <div class="brand-grid">
+            @foreach($brands as $brand)
+                <?php
+                    $current_first_letter = strtoupper(substr($brand->name, 0, 1));
+
+                if (!isset($header_first_letter) || (isset($header_first_letter) && $current_first_letter != $header_first_letter)) {
+                    echo '</ul> <h2>' . $current_first_letter . '</h2> <ul>';
+                }
+                $header_first_letter = $current_first_letter
+                                                                                                                ?>
 
 
-            @foreach($brands->chunk($chunk_size) as $chunk)
-                        <div class="col-md-4">
+                        <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/">{{ $brand->name }}</a>
 
-                            <ul>
-                                @foreach($chunk as $brand)
+                @endforeach
+            </div>
 
-                                                <?php
-                                    $current_first_letter = strtoupper(substr($brand->name, 0, 1));
+{{--            @foreach($brands->chunk($chunk_size) as $chunk)--}}
+{{--                        <div class="col-md-4">--}}
 
-                                    if (!isset($header_first_letter) || (isset($header_first_letter) && $current_first_letter != $header_first_letter)) {
-                                        echo '</ul>
-                                                                                                                                        <h2>' . $current_first_letter . '</h2>
-                                                                                                                                        <ul>';
-                                    }
-                                    $header_first_letter = $current_first_letter
-                                                                                                                                            ?>
+{{--                            <ul>--}}
+{{--                                @foreach($chunk as $brand)--}}
 
-                                                <li>
-                                                    <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/">{{ $brand->name }}</a>
-                                                </li>
+{{--                                                <?php--}}
+{{--                                    $current_first_letter = strtoupper(substr($brand->name, 0, 1));--}}
 
-                                @endforeach
-                            </ul>
+{{--                                    if (!isset($header_first_letter) || (isset($header_first_letter) && $current_first_letter != $header_first_letter)) {--}}
+{{--                                        echo '</ul>--}}
+{{--                                                                                                                                        <h2>' . $current_first_letter . '</h2>--}}
+{{--                                                                                                                                        <ul>';--}}
+{{--                                    }--}}
+{{--                                    $header_first_letter = $current_first_letter--}}
+{{--                                                                                                                                            ?>--}}
 
-                        </div>
-                        <?php
-                unset($header_first_letter);
-                                                                ?>
-            @endforeach
+{{--                                                <li>--}}
+{{--                                                    <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/">{{ $brand->name }}</a>--}}
+{{--                                                </li>--}}
+
+{{--                                @endforeach--}}
+{{--                            </ul>--}}
+
+{{--                        </div>--}}
+{{--                        <?php--}}
+{{--                unset($header_first_letter);--}}
+{{--                                                                ?>--}}
+{{--            @endforeach--}}
 
         </div>
 
