db914549aeb5a8e0ecc4d9380339d1b33402a7a2
RDeBont <rowandebont@outlook.com>
2025-09-16 12:41:22 +0200

    Ticket13 Lijst met letters van de merken voor snel opzoek megelijk heid

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -48,17 +48,29 @@ $topSplit = 1;
 
 
     <div class="jumbotron">
-        
 
         @php
+            $alphabet = range('A', 'Z');
             $groupedBrands = $brands->groupBy(function ($brand) {
                 return strtoupper(substr($brand->name, 0, 1));
             });
         @endphp
 
+        <div class="alphabet-nav">
+            <span>Ga naar letter:<br></span>
+            @foreach($alphabet as $letter)
+                @if($groupedBrands->has($letter))
+                    <a href="#letter-{{ $letter }}">{{ $letter }}</a>
+                @else
+                    <span class="disabled">{{ $letter }}</span>
+                @endif
+                @if(!$loop->last) - @endif
+            @endforeach
+        </div>
+
         <div class="brands-grid">
             @foreach($groupedBrands as $letter => $letterBrands)
-                <div class="brand-column">  
+                <div class="brand-column" id="letter-{{ $letter }}">
                     <h2 class="brand-letter">{{ $letter }}</h2>
                     @foreach($letterBrands as $brand)
                         <div class="brand-item">
